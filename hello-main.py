f3155703d6864cbc31ac387bf56d372eb414ac02
Zainamusleh <132077487+Zainamusleh@users.noreply.github.com>
2023-06-03 14:23:30 +0300

    Helllo
    a test

---
+++ hello-main.py
@@ -0,0 +1,10 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image.HEART)
+    sleep(1000)
+    display.scroll('zaina')
+    
